7ec3b17604f05e60a19aa3d545dfc874806a3018
InsaneMiner <gurued58@gmail.com>
2021-02-14 20:50:59 +1030

    Deleted one requorement from setup.py and requirements.txt

--- setup.py
+++ setup.py
@@ -11,29 +11,7 @@ def zipdir(path, ziph):
 name, version = ("lemon-framework", "1.2.1")
 
 
-if len(sys.argv) < 2:
-    
-	import re
-
-	from setuptools import setup, find_packages
-
-	setup(
-		name=name,
-		version=version,
-		author='Joel Schofield',
-		author_email='none@none.none',
-		url='https://github.com/InsaneMiner/Lemon',
-		license='LICENSE',
-		description='Web framework and web server written in python',
-		long_description=open('README.md').read(),
-		packages=["lemon","lemon.libs"],
-		package_data={'': ['default.zip']},
-		include_package_data=True,
-		install_requires = ["urllib2","regex","asyncio","watchdog","shutil","zipfile"],
-		
-		
-	)
-elif sys.argv[1] == "dev_build":
+if len(sys.argv) < 2 and sys.argv[1] == "dev_build":
 	zipf = zipfile.ZipFile('lemon/default.zip', 'w', zipfile.ZIP_DEFLATED)
 	zipdir('lemon/default', zipf)
 	zipf.close()
@@ -55,5 +33,5 @@ else:
 		packages=["lemon","lemon.libs"],
 		package_data={'': ['default.zip']},
 		include_package_data=True,
-		install_requires = ["urllib2","regex","asyncio","watchdog","shutil","zipfile"],
+		install_requires = ["urllib2","regex","asyncio","watchdog","shutil"],
 	)
